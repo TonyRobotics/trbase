dadcb8d99ff77e6a1211661ec176251c4e832af7
Your Name <you@example.com>
2018-11-02 08:55:12 +0000

    add ultrasound

--- trd_driver/udev/create_udev_rules.sh
+++ trd_driver/udev/create_udev_rules.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# use 'udevadm info --name=/dev/ttyUSB0' to show the USB port
+# and edit KERNELS=="x-x.x" in motor_trd.rules file
+
 echo "Creating /etc/udev/rules.d/motor_trd.rules"
 cp motor_trd.rules  /etc/udev/rules.d
 echo "Restarting udev"
